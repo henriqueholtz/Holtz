7804e7283560b3eeb64ac9800506b8bbca27c56a
henriqueholtz <henrique_holtz@hotmail.com>
2023-06-01 18:32:46 -0300

    chore: Rename "CQRS" project to "Holtz.CQRS.Api"

---
+++ CQRS/CQRS/Controllers/ProductssController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Holtz.CQRS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductssController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
