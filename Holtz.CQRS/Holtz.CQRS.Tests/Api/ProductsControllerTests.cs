ddaba45f471d2e6701bd04ceeb8fdcfab3325d0e
henriqueholtz <henrique_holtz@hotmail.com>
2023-06-03 09:32:15 -0300

    test: Add test project with one web api test

---
+++ Holtz.CQRS/Holtz.CQRS.Tests/Api/ProductsControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Holtz.CQRS.Api.Controllers;
+using Holtz.CQRS.Application.Queries.GetProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Holtz.CQRS.Tests.Api
+{
+    /// <summary>
+    /// https://www.learmoreseekmore.com/2022/02/dotnet6-unit-testing-aspnetcore-web-api-using-xunit.html
+    /// </summary>
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public async Task GetProducts()
+        {
+            // Arrange
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddMediatR(sfg => sfg.RegisterServicesFromAssembly(typeof(GetProductsQueryHandler).GetTypeInfo().Assembly));
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            IMediator mediator = serviceProvider.GetService<IMediator>();
+            ProductsController? controller = new ProductsController(mediator);
+
+            // Act
+            OkObjectResult result = (OkObjectResult)await controller.Index(new GetProductsQuery());
+
+
+            // Assert
+            Assert.True(result.StatusCode == 200);
+        }
+    }
+}
