6457783b57fdf674c15604d71b574244f235c926
henriqueholtz <henrique_holtz@hotmail.com>
2023-09-20 07:42:11 -0300

    feat: Holtz.WebSockets => Implementing web-scoket consumert (/ws)

---
+++ Holtz.WebSockets/Holtz.WebSockets.Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+using System.Text;
+
+Console.WriteLine("Initializing a consumer...");
+
+using var ws = new ClientWebSocket();
+
+await ws.ConnectAsync(new Uri("ws://localhost:5050/ws"), CancellationToken.None);
+
+byte[] buf = new byte[1056];
+
+var result = await ws.ReceiveAsync(buf, CancellationToken.None);
+
+if (result.MessageType == WebSocketMessageType.Close)
+{
+    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+    Console.WriteLine(result.CloseStatusDescription);
+}
+else
+{
+    Console.WriteLine(Encoding.ASCII.GetString(buf, 0, result.Count));
+}
+Console.ReadLine();
