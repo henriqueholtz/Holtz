250692e274e58182777d37d8fee9d3361be02889
Henrique Holtz <henrique_holtz@hotmail.com>
2024-01-30 07:17:40 -0300

    test: Holtz.PostgreSQL => Adding integration tests project with minial setup and its first test (testing "/api/home")

---
+++ Holtz.PostreSQL/Holtz.PostreSQL.Api.IntegrationTests/Setup/ApiWebApplicationFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Holtz.PostreSQL.Api.IntegrationTests.Setup
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<ProgramApiMarker>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.ConfigureTestServices(services =>
+            {
+
+            });
+        }
+    }
+}
---
+++ Holtz.PostreSQL/Holtz.PostreSQL.Api.IntegrationTests/Tests/HomeTests.cs
@@ -0,0 +1,22 @@
+﻿using Holtz.PostreSQL.Api.IntegrationTests.Setup;
+using System.Net;
+
+namespace Holtz.PostreSQL.Api.IntegrationTests.Tests
+{
+    public class HomeTests
+    {
+        [Fact]
+        public async Task GET_Home_ShouldBeOk()
+        {
+            // Arrange
+            await using var application = new ApiWebApplicationFactory();
+            using var client = application.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/home");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
