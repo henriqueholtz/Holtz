2c9b300876a2008f4ad4e9084aaa1a14c3d415e7
Henrique Holtz <henrique_holtz@hotmail.com>
2024-02-03 10:57:11 -0300

    test: Holtz.Refit => Add integration test for GET "/api/beers" simulating an error from external API

--- Holtz.Refit/Holtz.Refit.Api.IntegrationTests/Setup/ApiWebApplicationFactory.cs
+++ Holtz.Refit/Holtz.Refit.Api.IntegrationTests/Setup/ApiWebApplicationFactory.cs
@@ -1,15 +1,10 @@
-﻿using Holtz.Refit.Services.Interfaces;
+﻿using Holtz.Refit.Api.IntegrationTests.WireMockServers;
+using Holtz.Refit.Services.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Refit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WireMock.Server;
 
 namespace Holtz.Refit.Api.IntegrationTests.Setup
 {
@@ -22,7 +17,7 @@ namespace Holtz.Refit.Api.IntegrationTests.Setup
                 services.AddRefitClient<IRandomDataApi>()
                 .ConfigureHttpClient(c =>
                 {
-                    c.BaseAddress = new Uri("http://localhost:9876");
+                    c.BaseAddress = new Uri($"http://localhost:{RandomDataApiWireMockServer.DEFAULT_PORT}");
                     c.Timeout = TimeSpan.FromMilliseconds(1 * 1000);
                 });
             });
---
+++ Holtz.Refit/Holtz.Refit.Api.IntegrationTests/WireMockServers/RandomDataApiWireMockServer.cs
@@ -0,0 +1,7 @@
+﻿namespace Holtz.Refit.Api.IntegrationTests.WireMockServers
+{
+    internal class RandomDataApiWireMockServer
+    {
+        internal const int DEFAULT_PORT = 9876;
+    }
+}
