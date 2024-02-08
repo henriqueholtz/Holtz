d99c22cb52e35dcdf1942b0b1ea125038dfec316
Henrique Holtz <henrique_holtz@hotmail.com>
2024-02-07 21:25:07 -0300

    feat: Holtz.AspireStarter => Adding RabbitMq support as a side container

--- Holtz.AspireStarter/Holtz.AspireStarter.AppHost/Program.cs
+++ Holtz.AspireStarter/Holtz.AspireStarter.AppHost/Program.cs
@@ -1,8 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiservice = builder.AddProject<Projects.Holtz_AspireStarter_ApiService>("apiservice");
+var rabbitMQ = builder.AddRabbitMQContainer("message_with_rabbitMQ", password: "rabbitmqpass");
+
 
 builder.AddProject<Projects.Holtz_AspireStarter_Web>("webfrontend")
-    .WithReference(apiservice);
+    .WithReference(apiservice)
+    .WithReference(rabbitMQ);
 
 builder.Build().Run();
