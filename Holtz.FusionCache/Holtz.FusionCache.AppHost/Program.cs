e8220b84e472d95446e261a9b228024f95f4d01b
Henrique Holtz <henrique_holtz@hotmail.com>
2024-08-27 06:12:10 -0300

    feat: Holtz.FusionCache => Registering redis into .NET Aspire (AppHost project) as "redis-cache"

--- Holtz.FusionCache/Holtz.FusionCache.AppHost/Program.cs
+++ Holtz.FusionCache/Holtz.FusionCache.AppHost/Program.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Holtz_FusionCache_Api>("holtz-fusioncache-api");
+// Add a Redis server to the application.
+var redisCache = builder.AddRedis("redis-cache"); // OOH, ADD REDIS? NICE 
+
+builder.AddProject<Projects.Holtz_FusionCache_Api>("holtz-fusioncache-api")
+    .WithReference(redisCache);
 
 builder.Build().Run();
