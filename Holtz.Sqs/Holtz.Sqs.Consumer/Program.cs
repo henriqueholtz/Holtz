e5d36652751f702dd50745125068c61cd6dd0e05
Henrique Holtz <henrique_holtz@hotmail.com>
2024-09-24 07:32:49 -0300

    feat: Holtz.Sqs => Implementing the consumer

--- Holtz.Sqs/Holtz.Sqs.Consumer/Program.cs
+++ Holtz.Sqs/Holtz.Sqs.Consumer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Holtz.Sqs.Shared.Messages;
+
+var cancellationTokenSource = new CancellationTokenSource();
+Console.WriteLine("Starting Holtz.Sqs.Consumer...");
+
+var sqsClient = new AmazonSQSClient();
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string> { "All" },
+    MessageAttributeNames = new List<string> { "All" }
+};
+
+while (!cancellationTokenSource.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cancellationTokenSource.Token);
+    foreach (Message message in response.Messages)
+    {
+        Console.WriteLine($"Message ID: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(3000);
+}
