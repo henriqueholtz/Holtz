55d7e81dc5102ca8b59045a03ad9a7ed3c7e2dc9
Henrique Holtz <henrique_holtz@hotmail.com>
2024-09-24 06:50:58 -0300

    feat: Holtz.Sqs => Creating the publisher project

---
+++ Holtz.Sqs/Holtz.Sqs.Publisher/Messages/CustomerCreated.cs
@@ -0,0 +1,10 @@
+namespace Holtz.Sqs.Publisher.Messages;
+
+public class CustomerCreated
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+    public required string Email { get; init; }
+    public required string GithubUsername { get; init; }
+    public required DateTime BirthDate { get; init; }
+}
---
+++ Holtz.Sqs/Holtz.Sqs.Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Holtz.Sqs.Publisher.Messages;
+
+Console.WriteLine("Starting Holtz.Sqs.Publisher...");
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Name = "Henrique Holtz",
+    Email = "henrique_holtz@hotmail.com",
+    BirthDate = new DateTime(1999, 10, 07),
+    GithubUsername = "henriqueholtz"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine("Message sent!");
+Console.ReadLine();
