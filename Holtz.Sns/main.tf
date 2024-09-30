75e0e309de4af63d2e68ccc49f09f4fda26b9e5f
Henrique Holtz <henrique_holtz@hotmail.com>
2024-09-30 19:34:09 -0300

    feat: Holtz.Sqs => Adding a second aws sqs queue and its policy to receive messages from a SNS topic

--- Holtz.Sns/main.tf
+++ Holtz.Sns/main.tf
@@ -1,3 +1,9 @@
+# Creating an AWS SQS queue as standard (DLQ)
+resource "aws_sqs_queue" "deadletter_customers_queue" {
+  name  = "customers-dlq"
+  message_retention_seconds = 1209600 # 14 days
+}
+
 # Creating the main AWS SQS queue as standard
 resource "aws_sqs_queue" "customers_queue" {
   name  = "customers"
@@ -32,16 +38,47 @@ resource "aws_sqs_queue_policy" "customers_queue_policy" {
     })
 }
 
-# Creating a second AWS SQS queue as standard (DLQ)
-resource "aws_sqs_queue" "deadletter_customers_queue" {
-  name  = "customers-dlq"
-  message_retention_seconds = 1209600 # 14 days
+# Creating a second AWS SQS queue as standard
+resource "aws_sqs_queue" "customers2_queue" {
+  name  = "customers2"
+  redrive_policy = jsonencode({
+                    "deadLetterTargetArn" = aws_sqs_queue.deadletter_customers_queue.arn,
+                    "maxReceiveCount" = 3
+                  })
 }
 
+# Creating a policy to sqs queue to allow SNS publish a message into it
+resource "aws_sqs_queue_policy" "customers2_queue_policy" {
+  queue_url = aws_sqs_queue.customers2_queue.id
+  policy = jsonencode(
+        {
+            "Version": "2012-10-17",
+            "Id": "sqspolicy",
+            "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "sns.amazonaws.com"
+                },
+                "Action": "sqs:SendMessage",
+                "Resource": aws_sqs_queue.customers2_queue.arn,
+                "Condition": {
+                    "ArnEquals": {
+                        "aws:SourceArn": aws_sns_topic.customers_topic.arn
+                    }
+                }
+            }
+        ]
+    })
+}
+
+
+# Creating the SNS topic
 resource "aws_sns_topic" "customers_topic" {
   name = "customers_topic"
 }
 
+# Creating the subscription between the SNS topic and the SQS queue
 resource "aws_sns_topic_subscription" "sns_customers_topic_subscription" {
   topic_arn = aws_sns_topic.customers_topic.arn
   protocol  = "sqs"
