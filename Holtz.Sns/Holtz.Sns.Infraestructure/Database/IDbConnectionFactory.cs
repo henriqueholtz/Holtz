a254e3dfe3d66d3aabc80b2b8f61deca8c914d8a
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-01 06:40:26 -0300

    feat: Holtz.Sns => Creating the "Infraestructure" project/layer

---
+++ Holtz.Sns/Holtz.Sns.Infraestructure/Database/IDbConnectionFactory.cs
@@ -0,0 +1,8 @@
+using System.Data;
+
+namespace Holtz.Sns.Infraestructure.Database;
+
+public interface IDbConnectionFactory
+{
+    public Task<IDbConnection> CreateConnectionAsync();
+}
---
+++ Holtz.Sns/Holtz.Sns.Infraestructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,58 @@
+
+using Dapper;
+using Holtz.Sns.Domain;
+using Holtz.Sns.Domain.Interfaces;
+using Holtz.Sns.Infraestructure.Database;
+
+namespace Holtz.Sns.Infraestructure.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly IDbConnectionFactory _connectionFactory;
+
+    public CustomerRepository(IDbConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task<bool> CreateAsync(Customer customer, CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        var result = await connection.ExecuteAsync(
+            @"INSERT INTO Customers (Id, GitHubUsername, FullName, Email, BirthDate) 
+            VALUES (@Id, @GitHubUsername, @FullName, @Email, @BirthDate)",
+            customer);
+        return result > 0;
+    }
+
+    public async Task<Customer?> GetAsync(Guid id, CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        return await connection.QuerySingleOrDefaultAsync<Customer>(
+            "SELECT * FROM Customers WHERE Id = @Id LIMIT 1", new { Id = id });
+    }
+
+    public async Task<IEnumerable<Customer>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        return await connection.QueryAsync<Customer>("SELECT * FROM Customers");
+    }
+
+    public async Task<bool> UpdateAsync(Customer customer, CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        var result = await connection.ExecuteAsync(
+            @"UPDATE Customers SET GitHubUsername = @GitHubUsername, FullName = @FullName, Email = @Email, 
+                 BirthDate = @BirthDate WHERE Id = @Id",
+            customer);
+        return result > 0;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        using var connection = await _connectionFactory.CreateConnectionAsync();
+        var result = await connection.ExecuteAsync(@"DELETE FROM Customers WHERE Id = @Id",
+            new { Id = id });
+        return result > 0;
+    }
+}
