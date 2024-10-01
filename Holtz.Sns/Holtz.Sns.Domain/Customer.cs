5d5b34a824fe75b0996178dd4afc16f94e3d2ade
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-01 06:37:12 -0300

    feat: Holtz.Sns => Creating the "Domain" project/layer

---
+++ Holtz.Sns/Holtz.Sns.Domain/Customer.cs
@@ -0,0 +1,10 @@
+namespace Holtz.Sns.Domain;
+
+public class Customer
+{
+    public required Guid Id { get; init; } = Guid.NewGuid();
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required string GitHubUsername { get; init; }
+    public required DateTime BirthDate { get; init; }
+}
---
+++ Holtz.Sns/Holtz.Sns.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+namespace Holtz.Sns.Domain.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<bool> CreateAsync(Customer customer, CancellationToken cancellationToken);
+
+    Task<Customer?> GetAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<IEnumerable<Customer>> GetAllAsync(CancellationToken cancellationToken);
+
+    Task<bool> UpdateAsync(Customer customer, CancellationToken cancellationToken);
+
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
