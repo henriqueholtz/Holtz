3fb2ab6273e3651c2679d31c1004ea183b156e75
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-04 06:54:38 -0300

    feat: Holtz.DynamoDb.Api => Adding validation middleware and configuring the last DI

--- Holtz.DynamoDb/Holtz.DynamoDb.Api/Program.cs
+++ Holtz.DynamoDb/Holtz.DynamoDb.Api/Program.cs
@@ -1,17 +1,36 @@
+using Amazon.DynamoDBv2;
+using FluentValidation.AspNetCore;
+using Holtz.DynamoDb.Api.Middlewares;
 using Holtz.DynamoDb.Application;
 using Holtz.DynamoDb.Application.Interfaces;
 using Holtz.DynamoDb.Domain.Interfaces;
+using Holtz.DynamoDb.Infraestructure.Repositories;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddFluentValidationAutoValidation(x =>
+{
+    x.DisableDataAnnotationsValidation = true;
+}); ;
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
+builder.Services.AddScoped<IAmazonDynamoDB, AmazonDynamoDBClient>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IGitHubService, GitHubService>();
-// builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddHttpClient("GitHub", httpClient =>
+{
+    httpClient.BaseAddress = new Uri(builder.Configuration.GetValue<string>("GitHub:ApiBaseUrl")!);
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.Accept, "application/vnd.github.v3+json");
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.UserAgent, $"Course-{Environment.MachineName}");
+});
 
 var app = builder.Build();
 
@@ -22,6 +41,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ValidationExceptionMiddleware>();
 app.MapControllers();
 app.UseHttpsRedirection();
 
