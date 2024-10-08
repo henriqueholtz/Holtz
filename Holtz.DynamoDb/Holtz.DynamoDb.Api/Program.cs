5b5b104f898e25a408af0e9fa27d19e5e84fd057
Henrique Holtz <henrique_holtz@hotmail.com>
2024-10-08 07:56:40 -0300

    feat: Holtz.DynamoDb => Adding support to run the database through Docker instead of run it at AWS cloud

--- Holtz.DynamoDb/Holtz.DynamoDb.Api/Program.cs
+++ Holtz.DynamoDb/Holtz.DynamoDb.Api/Program.cs
@@ -1,4 +1,5 @@
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using FluentValidation.AspNetCore;
 using Holtz.DynamoDb.Api.Middlewares;
 using Holtz.DynamoDb.Application;
@@ -13,12 +14,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFluentValidationAutoValidation(x =>
 {
     x.DisableDataAnnotationsValidation = true;
-}); ;
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IAmazonDynamoDB, AmazonDynamoDBClient>();
+#region Configure DynamoDb
+if (bool.TryParse(builder.Configuration["DynamoDb:UseLocal"]?.ToString() ?? "false", out bool useDynamoDbLocal) && useDynamoDbLocal)
+{
+    // Requires DynamoDb running locally. See the README for more details.
+    var dynamoDbConfig = new AmazonDynamoDBConfig
+    {
+        ServiceURL = builder.Configuration["DynamoDb:Url"]?.ToString() ?? "http://localhost:8000"
+    };
+    builder.Services.AddScoped<IAmazonDynamoDB, AmazonDynamoDBClient>(sp => new AmazonDynamoDBClient(dynamoDbConfig));
+}
+else
+{
+    // Will use your credentials from your AWS CLI, and the DynamoDb on AWS Cloud.
+    builder.Services.AddScoped<IAmazonDynamoDB, AmazonDynamoDBClient>();
+}
+#endregion
+
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IGitHubService, GitHubService>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
@@ -37,6 +54,68 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    #region Setting up dynamoDb table
+    if (useDynamoDbLocal)
+    {
+        string tableName = "customers";
+        await using (var scope = app.Services.CreateAsyncScope())
+        {
+            var _dynamoDbClient = scope.ServiceProvider.GetRequiredService<IAmazonDynamoDB>();
+
+            // verify if the table already exists
+            var response = await _dynamoDbClient.DescribeTableAsync(new DescribeTableRequest
+            {
+                TableName = tableName
+            });
+            if (response.Table.TableStatus != TableStatus.ACTIVE)
+            {
+                string pkName = "pk";
+                string skName = "sk";
+                // Define table structure and create it
+                var request = new CreateTableRequest
+                {
+                    TableName = tableName,
+                    AttributeDefinitions = new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition
+                        {
+                            AttributeName = pkName,  // Define the primary key attribute
+                            AttributeType = "S"    // 'S' stands for string type
+                        },
+                        new AttributeDefinition
+                        {
+                            AttributeName = skName,  // Define the primary key attribute
+                            AttributeType = "S"      // 'S' stands for string type
+                        },
+                    },
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement
+                        {
+                            AttributeName = pkName,   // Partition Key
+                            KeyType = "HASH"          // Define as Partition Key
+                        },
+                        new KeySchemaElement
+                        {
+                            AttributeName = skName,   // Sort Key
+                            KeyType = "RANGE"         // Define as Sort Key
+                        }
+                    },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 5,    // Set read capacity
+                        WriteCapacityUnits = 5    // Set write capacity
+                    }
+                };
+                Console.WriteLine($"Creating the table {tableName} on DynamoDb locally");
+                var responseCreate = await _dynamoDbClient.CreateTableAsync(request);
+                Console.WriteLine($"Table creation status: {responseCreate.TableDescription.TableStatus}");
+            }
+
+        }
+    }
+    #endregion
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
--- Holtz.DynamoDb/README.md
+++ Holtz.DynamoDb/README.md
@@ -7,17 +7,28 @@
 ## Requirements
 
 - .NET 8+
-- AWS account
-- AWS CLI
-- Terraform
+- AWS account + AWS CLI + Terraform or just Docker
 
-### Setup
+### Setup using AWS + Terraform
 
 1. Create an IAM user on your own AWS account with the role `AmazonDynamoDBFullAccess`. After creating the user, generate its access key;
 2. Install all the requirements;
 3. Configure your AWS CLI with the access key using `aws configure`;
 4. Initialize your terraform using `terraform init`;
 5. Apply your terraform using `terraform apply` (you need to verify carefully and type "yes" and only then the resources will be created on AWS)
+6. Make sure the setting `DynamoDb:UseLocal` is disabled at `appsettings.json`/`appsettings.Development.json`
+7. Ready! Just run the `Holtz.DynamoDb.Api` project and use it.
+
+### Setup just using Docker
+
+1. Run the DynamoDb as a docker container ([ref](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html)). Ex:
+
+   ```
+   docker run --rm -p 8000:8000 amazon/dynamodb-local
+   ```
+
+2. Make sure the setting `DynamoDb:UseLocal` is enabled at `appsettings.json`/`appsettings.Development.json`
+3. Ready! Just run the `Holtz.DynamoDb.Api` project and use it.
 
 ### Teardown
 
@@ -29,3 +40,7 @@ Also you should want to remove the user and/or its access key.
 
 - `aws configure`
 - `aws dynamodb list-tables`
+
+### TODOs
+
+- Add tests like unit and/or integration and/or arch tests
