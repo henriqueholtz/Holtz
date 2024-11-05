bc054b8abfdc0ec06223f13cf9a0ca027ea38ab5
Henrique Holtz <henrique_holtz@hotmail.com>
2024-11-05 07:46:50 -0300

    test: Holtz.DesignPattern.AbstractFactoryTests => Testing Main method

--- Holtz.DesignPattern.AbstractFactory/Holtz.DesignPattern.AbstractFactory/Program.cs
+++ Holtz.DesignPattern.AbstractFactory/Holtz.DesignPattern.AbstractFactory/Program.cs
@@ -1,32 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Holtz.DesignPattern.AbstractFactory.Domain.Enums;
+﻿using Holtz.DesignPattern.AbstractFactory.Domain.Enums;
 using Holtz.DesignPattern.AbstractFactory.Domain.Products;
 using Holtz.DesignPattern.AbstractFactory.Factory.AbstractFactory;
+using System.Runtime.CompilerServices;
 
-static void ShowDetails(DoughBase doughBase) => Console.WriteLine($"Type is {doughBase.Name}. Name is {doughBase.Name}. Ingredients: {string.Join(",", doughBase.Ingredients.ToArray())}... \n");
+[assembly: InternalsVisibleTo("Holtz.DesignPattern.AbstractFactoryTests")]
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        static void ShowDetails(DoughBase doughBase) => Console.WriteLine($"Type is {doughBase.Name}. Ingredients: {string.Join(",", doughBase.Ingredients.ToArray())}...");
 
-Console.WriteLine("Hello, Starting...");
-Console.WriteLine();
-Console.WriteLine();
+        Console.WriteLine("Hello, Starting...");
 
-// Get Factories
-DoughAbstractFactory factoryCake = DoughAbstractFactory.CreateFactoryDough(EnumDough.Cake);
-DoughAbstractFactory factoryPizza = DoughAbstractFactory.CreateFactoryDough(EnumDough.Pizza);
+        // Get Factories
+        DoughAbstractFactory factoryCake = DoughAbstractFactory.CreateFactoryDough(EnumDough.Cake);
+        DoughAbstractFactory factoryPizza = DoughAbstractFactory.CreateFactoryDough(EnumDough.Pizza);
 
-// Create objects based on cake's type
-DoughBase cakeChocolate = factoryCake.CreateDough((EnumDough)EnumCake.Chocolate);
-DoughBase cakeOrange = factoryCake.CreateDough((EnumDough)EnumCake.Orange);
+        // Create objects based on cake's type
+        DoughBase cakeChocolate = factoryCake.CreateDough((EnumDough)EnumCake.Chocolate);
+        DoughBase cakeOrange = factoryCake.CreateDough((EnumDough)EnumCake.Orange);
 
-// Create objects based on pizza's type
-DoughBase pizzaPepperoni = factoryPizza.CreateDough((EnumDough)EnumPizza.Pepperoni);
-DoughBase pizzaBacon = factoryPizza.CreateDough((EnumDough)EnumPizza.Bacon);
+        // Create objects based on pizza's type
+        DoughBase pizzaPepperoni = factoryPizza.CreateDough((EnumDough)EnumPizza.Pepperoni);
+        DoughBase pizzaBacon = factoryPizza.CreateDough((EnumDough)EnumPizza.Bacon);
 
-// Show details
-ShowDetails(cakeChocolate);
-ShowDetails(cakeOrange);
-ShowDetails(pizzaPepperoni);
-ShowDetails(pizzaBacon);
+        // Show details
+        ShowDetails(cakeChocolate);
+        ShowDetails(cakeOrange);
+        ShowDetails(pizzaPepperoni);
+        ShowDetails(pizzaBacon);
 
-Console.WriteLine();
-Console.WriteLine("Finished.");
-Console.ReadLine();
+        Console.WriteLine("Finished.");
+    }
+}
---
+++ Holtz.DesignPattern.AbstractFactory/Holtz.DesignPattern.AbstractFactoryTests/ProgramTests.cs
@@ -0,0 +1,34 @@
+using System.Text;
+
+namespace Holtz.DesignPattern.AbstractFactoryTests;
+
+public class ProgramTests
+{
+    StringBuilder _consoleOutput = new StringBuilder();
+    public ProgramTests()
+    {
+        var consoleOutputWriter = new StringWriter(_consoleOutput);
+        Console.SetOut(consoleOutputWriter);
+    }
+
+    [Fact]
+    public void MainTest()
+    {
+        var outputLines = RunMainAndGetConsoleOutput();
+
+        Assert.Equal(7, outputLines.Length);
+        Assert.Equal("Hello, Starting...", outputLines[0]);
+        Assert.Equal("Type is Cake of chocolate. Ingredients: Sugar,Chocolate...", outputLines[1]);
+        Assert.Equal("Type is Cake of orange. Ingredients: Sugar,Orange...", outputLines[2]);
+        Assert.Equal("Type is Pizza of pepperoni. Ingredients: Pepperoni,Dough...", outputLines[3]);
+        Assert.Equal("Type is Pizza of bacon. Ingredients: Bacon,Dough...", outputLines[4]);
+        Assert.Equal("Finished.", outputLines[5]);
+        Assert.Equal("", outputLines[6]);
+    }
+
+    private string[] RunMainAndGetConsoleOutput()
+    {
+        Program.Main(new string[] { });
+        return _consoleOutput.ToString().Split("\r\n");
+    }
+}
