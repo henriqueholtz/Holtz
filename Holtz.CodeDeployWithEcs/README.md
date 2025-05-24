4c9e0923c70e087229dbede2e2b5b3f6d6552494
Henrique Holtz <henrique_holtz@hotmail.com>
2025-05-24 16:41:31 -0300

    feat: Holtz.CodeDeployWithEcs => Adding terraform scripts including VPC, ALB, ECS and needed components like IAM Roles

---
+++ Holtz.CodeDeployWithEcs/README.md
@@ -0,0 +1,9 @@
+# Holtz.CodeDeployWithEcs
+
+AWS Required Permissions 
+
+- `AmazonECS_FullAccess`
+- `AmazonVPCFullAccess`
+- `IAMFullAccess`
+- `ElasticLoadBalancingFullAccess`
+- `CloudWatchLogsFullAccess`
---
+++ Holtz.CodeDeployWithEcs/Terraform/ECS-Fargate-arm64/iam.tf
@@ -0,0 +1,76 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-codedeploy-task-execution-role"
+
+  assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+          }
+        }
+      ]
+    })
+}
+
+resource "aws_iam_role" "vpc_lattice_role" {
+  name = "holtz_codedeploy_ecsInfrastructureRole"
+
+  assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+            "Sid": "AllowAccessToECSForInfrastructureManagement", 
+            "Effect": "Allow", 
+            "Principal": {
+                "Service": "ecs.amazonaws.com" 
+            }, 
+            "Action": "sts:AssumeRole" 
+        }
+      ]
+    })
+}
+
+
+resource "aws_iam_role_policy" "ecs_task_execution_inline_policy" {
+  name   = "ecs_task_execution_ssmmessages_policy"
+  role   = aws_iam_role.ecs_task_execution_role.name
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "ssmmessages:*",
+            "Resource": "*"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", 
+  ])
+  policy_arn = each.value
+
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "vpc_lattice_policy" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", 
+    "arn:aws:iam::aws:policy/AmazonECSInfrastructureRolePolicyForVpcLattice",
+    "arn:aws:iam::aws:policy/service-role/AmazonECSInfrastructureRolePolicyForVolumes",
+    "arn:aws:iam::aws:policy/service-role/AmazonECSInfrastructureRolePolicyForServiceConnectTransportLayerSecurity"
+    # "arn:aws:iam::aws:policy/AmazonEC2FullAccess", 
+    # "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  ])
+  policy_arn = each.value
+
+  role       = aws_iam_role.vpc_lattice_role.name
+}
