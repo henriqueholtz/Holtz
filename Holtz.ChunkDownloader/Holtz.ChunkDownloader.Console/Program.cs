1daa5bc5c8e8ac8f5914cdfda8c10184c1659644
Henrique Holtz <henrique_holtz@hotmail.com>
2025-05-28 14:40:44 -0300

    feat: Holtz.ChunkDownloader => Downloading multiple chunks

--- Holtz.ChunkDownloader/Holtz.ChunkDownloader.Console/Program.cs
+++ Holtz.ChunkDownloader/Holtz.ChunkDownloader.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Starting Holtz.ChunkDownloader...");
 Console.WriteLine();
 Console.WriteLine("Here is a example for base URL: https://example.com/chunks/my-id-video-segment-{0}.ts");
+Console.WriteLine();
 
 string baseUrl = "";
 Console.Write("Enter the base URL for the chunk files: ");
@@ -8,58 +9,60 @@ baseUrl = Console.ReadLine()?.Trim() ?? "";
 
 while (string.IsNullOrEmpty(baseUrl))
 {
+    Console.WriteLine();
     Console.Write("Base URL cannot be empty. Please enter a valid base URL: ");
     baseUrl = Console.ReadLine()?.Trim() ?? "";
 }
 
 Console.WriteLine();
-
-string contentType = "";
 Console.Write("Enter the Content-Type for the chunk files: ");
-contentType = Console.ReadLine()?.Trim() ?? "";
+string contentType = Console.ReadLine()?.Trim() ?? "";
 
-while (string.IsNullOrEmpty(contentType))
-{
-    Console.Write("Content-Type cannot be empty. Please enter a valid Content-Type: ");
-    contentType = Console.ReadLine()?.Trim() ?? "";
-}
-
-string downloadName = "";
+Console.WriteLine();
 Console.Write("Enter the Download Name (the chunks will be downloaded into this folder): ");
-downloadName = Console.ReadLine()?.Trim() ?? Guid.NewGuid().ToString();
+string downloadName = Console.ReadLine()?.Trim() ?? Guid.NewGuid().ToString();
 
 // TODO: Ask for the next fields
 int startIndex = 1;
+int maxLoops = 100 * 1000;
+int delayInMilliseconds = 2000;
 string fileExtension = ".ts";
 
 Console.WriteLine("Url: " + string.Format(baseUrl, startIndex));
 Console.WriteLine("Content-Type: " + contentType);
-
+Console.WriteLine("Starting chunk downloads...");
+Console.WriteLine();
 
 using (var client = new HttpClient())
 {
+    if (!string.IsNullOrWhiteSpace(contentType))
+        client.DefaultRequestHeaders.Add("Accept", contentType);
     try
     {
-        // TODO: Foreach loop to download multiple chunks
-        client.DefaultRequestHeaders.Add("Accept", contentType);
-        string url = string.Format(baseUrl, startIndex);
-        Console.WriteLine("Downloading from URL: " + url);
-        var response = await client.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-
-        string baseDirectory = Path.Combine(".downloads", downloadName);
-        // string fileName = Path.Combine(Directory.GetCurrentDirectory(), downloadName, startIndex + fileExtension);
-        if (!Directory.Exists(baseDirectory))
-            Directory.CreateDirectory(baseDirectory);
-
-        string filePath = Path.Combine(baseDirectory, startIndex + fileExtension);
-        Console.WriteLine("Saving to file: " + filePath);
-        using (var stream = response.Content.ReadAsStream())
-        using (var file = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+        for (int i = startIndex; i < maxLoops; i++)
         {
-            stream.CopyTo(file);
+            Console.WriteLine($"Downloading chunk {i} of {maxLoops}...");
+            string url = string.Format(baseUrl, i);
+
+            Console.WriteLine(" => Downloading from: " + url);
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string baseDirectory = Path.Combine(".downloads", downloadName);
+            if (!Directory.Exists(baseDirectory))
+                Directory.CreateDirectory(baseDirectory);
+
+            string filePath = Path.Combine(baseDirectory, i + fileExtension);
+            Console.WriteLine(" => Saving to file: " + filePath);
+            using (var stream = response.Content.ReadAsStream())
+            using (var file = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                stream.CopyTo(file);
+            }
+            Console.WriteLine(" => Download completed successfully.");
+            Console.WriteLine();
+            Thread.Sleep(delayInMilliseconds);
         }
-        Console.WriteLine("Download completed successfully.");
     }
     catch (Exception ex)
     {
