b41c68568c1f45419ecd19f94e5e7d2a8cc8c2a3
Henrique Holtz <henrique_holtz@hotmail.com>
2026-06-19 08:11:29 -0300

    chore: Holtz.Adk => Adding example to call anthropic with litellm

--- Holtz.Adk/agents/my_agent/agent.py
+++ Holtz.Adk/agents/my_agent/agent.py
@@ -1,4 +1,5 @@
 from google.adk import Agent
+from google.adk.models.lite_llm import LiteLlm
 
 root_agent = Agent(
     name="my_agent",
@@ -6,4 +7,5 @@ root_agent = Agent(
         You are a Python developer
     """,
     model="gemini-2.5-flash-lite"
+    # model=LiteLlm("anthropic/claude-sonnet-4-6")
 )
